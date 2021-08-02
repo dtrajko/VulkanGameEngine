3aed8c20fbb46211be9a7d9d4d75b4f104288794
Dejan Trajkovic <dtrajko@gmail.com>
2021-08-02 16:53:34 +0200

    17. Loading 3D Models (part 2)

--- VulkanGameEngine/lve_model.cpp
+++ VulkanGameEngine/lve_model.cpp
@@ -7,6 +7,7 @@
 // std
 #include <cassert>
 #include <cstring>
+#include <iostream>
 
 
 namespace lve {
@@ -27,6 +28,16 @@ namespace lve {
 			vkDestroyBuffer(lveDevice.device(), indexBuffer, nullptr);
 			vkFreeMemory(lveDevice.device(), indexBufferMemory, nullptr);
 		}
+	}
+
+	std::unique_ptr<LveModel> LveModel::createModelFromFile(LveDevice& device, const std::string& filepath)
+	{
+		Builder builder{};
+		builder.loadModel(filepath);
+
+		std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+
+		return std::make_unique<LveModel>(device, builder);
 	}
 
 	void LveModel::createVertexBuffers(const std::vector<Vertex>& vertices)
@@ -151,6 +162,72 @@ namespace lve {
 		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
 		attributeDescriptions[1].offset = offsetof(Vertex, color);
 		return attributeDescriptions;
+	}
+
+	void LveModel::Builder::loadModel(const std::string& filepath)
+	{
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str()))
+		{
+			throw std::runtime_error(warn + err);
+		}
+
+		vertices.clear();
+		indices.clear();
+
+		for (const auto& shape : shapes)
+		{
+			for (const auto& index : shape.mesh.indices)
+			{
+				Vertex vertex{};
+
+				if (index.vertex_index >= 0)
+				{
+					vertex.position = {
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2],
+					};
+				}
+
+				auto colorIndex = 3 * index.vertex_index + 2;
+				if (colorIndex < attrib.colors.size())
+				{
+					vertex.color = {
+						attrib.colors[colorIndex - 2],
+						attrib.colors[colorIndex - 1],
+						attrib.colors[colorIndex - 0],
+					};
+				}
+				else
+				{
+					vertex.color = { 1.0f, 1.0f, 1.0f }; // set default color
+				}
+
+				if (index.normal_index >= 0)
+				{
+					vertex.normal = {
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2],
+					};
+				}
+
+				if (index.texcoord_index >= 0)
+				{
+					vertex.uv = {
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1],
+					};
+				}
+
+				vertices.push_back(vertex);
+			}
+		}
 	}
 
 } // namespace lve
--- VulkanGameEngine/lve_model.hpp
+++ VulkanGameEngine/lve_model.hpp
@@ -8,6 +8,7 @@
 #include <glm/glm.hpp>
 
 // std
+#include <memory>
 #include <vector>
 
 
@@ -18,8 +19,10 @@ namespace lve {
 	public:
 
 		struct Vertex {
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
@@ -28,6 +31,8 @@ namespace lve {
 		struct Builder {
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 
 		LveModel(LveDevice& device, const LveModel::Builder& builder);
@@ -35,6 +40,8 @@ namespace lve {
 
 		LveModel(const LveModel&) = delete;
 		LveModel& operator=(const LveModel&) = delete;
+
+		static std::unique_ptr<LveModel> createModelFromFile(LveDevice& device, const std::string& filepath);
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
